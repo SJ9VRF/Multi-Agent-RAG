5999356377008131451d275cd3222967367527bd
Arefeh Yavary <yavary.rf@gmail.com>
2024-08-10 11:09:39 -0700

    Update text_utils.py

--- src/text_utils.py
+++ src/text_utils.py
@@ -1,1 +1,6 @@
 # Text processing utilities
+# text_utils.py
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def get_text_splitter():
+    return RecursiveCharacterTextSplitter(separators=["\n\n", "\n", "\r", "\t"])
