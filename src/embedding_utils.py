9446590b17919936cdf47fb10bff4a5688811dcc
Arefeh Yavary <yavary.rf@gmail.com>
2024-08-10 11:09:14 -0700

    Update embedding_utils.py

--- src/embedding_utils.py
+++ src/embedding_utils.py
@@ -1,1 +1,6 @@
 # Functions to handle embeddings
+# embedding_utils.py
+from chromadb.utils import embedding_functions
+
+def get_openai_embedding_function(api_key):
+    return embedding_functions.OpenAIEmbeddingFunction(api_key=api_key)
