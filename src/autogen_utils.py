90cf5f7079ba435628f72c4aaaf36042156eb103
Arefeh Yavary <yavary.rf@gmail.com>
2024-08-10 11:08:54 -0700

    Update autogen_utils.py

--- src/autogen_utils.py
+++ src/autogen_utils.py
@@ -1,1 +1,8 @@
 # Helper functions for AutoGen setup
+# autogen_utils.py
+import json
+
+def load_config(path='config/config.json'):
+    with open(path, 'r') as config_file:
+        config = json.load(config_file)
+    return config
